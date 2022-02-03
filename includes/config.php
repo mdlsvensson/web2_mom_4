781984d5dd1eb32cde04d2a61456dec90507239f
mdlsvensson <wilmer.lindau@gmail.com>
2022-02-03 14:35:02 +0100

    config.php comments

--- includes/config.php
+++ includes/config.php
@@ -8,10 +8,12 @@
   error_reporting(-1);
   ini_set("display_errors", 1);
 
+  // Autoload classes
   spl_autoload_register(function ($class_name) {
     include 'classes/' . $class_name . '.class.php';
   });
 
+  // If no session exists, start session
   if (!isset($_SESSION)) {
     session_start();
   }
