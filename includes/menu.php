cc9ab2820d30a828ef010914b3ddfc97d122c73f
mdlsvensson <wilmer.lindau@gmail.com>
2022-02-03 14:36:25 +0100

    menu.php comments

--- includes/menu.php
+++ includes/menu.php
@@ -3,10 +3,12 @@
                     <li><a href="index.php">Latest blog posts</a></li>
                     <li><a href="blog.php">All blog posts</a></li>
                     <?php
+                        // Show login and register links if there is no logged in user
                         if (!isset($_SESSION['email'])) {
                             echo '<li><a href="login.php">Log in</a></li>';
                             echo '<li><a href="register.php">Register</a></li>';
                         } else {
+                            // Show admin and logout links if there is a logged in user.
                             echo '<li><a href="admin.php">Admin</a></li>';
                             echo '<li><a href="logout.php">Log out</a></li>';
                         }
