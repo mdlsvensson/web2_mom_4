fb41b32d5976b1e47816d1e294d5d72f82586c50
mdlsvensson <wilmer.lindau@gmail.com>
2022-02-03 14:37:05 +0100

    index.php comments

--- index.php
+++ index.php
@@ -2,7 +2,9 @@
   $page_title = "Startsida";
   include("includes/header.php");
 
+  // Create new post Object
   $posts = new Post();
+  // Get the latest 2 posts
   $posts->getPosts(2);
 
   include("includes/footer.php");
