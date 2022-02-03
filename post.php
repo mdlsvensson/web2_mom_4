2938e1826c7bdfed09bb05a520fbc1563622513f
mdlsvensson <wilmer.lindau@gmail.com>
2022-02-03 14:44:25 +0100

    post.php comments

--- post.php
+++ post.php
@@ -3,11 +3,15 @@
   include("includes/header.php");
 
   if (isset($_GET['postid'])) {
+    // Get GET data
     $id = $_GET['postid'];
 
+    // Create new Post Obeject
     $post = new Post();
+    // Get post by id
     $post->getPostById($id);
   } else {
+    // If no GET data, redirect to index
     header('Location: index.php');
   }
 
