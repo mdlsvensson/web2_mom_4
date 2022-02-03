625596f0c5d558d2c10a76766dac1c9d54dc0167
mdlsvensson <wilmer.lindau@gmail.com>
2022-02-03 14:40:10 +0100

    admin.php comments

--- admin.php
+++ admin.php
@@ -17,18 +17,24 @@
 
   <?php
 
+    // If new post form is submitted
     if (isset($_POST['title'])) {
+      // Get POST data
       $title = $_POST['title'];
       $postContent = $_POST['postcontent'];
 
+      // Create new Post Object
       $post = new Post();
 
+      // Get any form validation errors
       $errors = $post->validate($title, $postContent);
 
+      // If there are errors write them to the DOM
       if (count($errors) > 0) {
         for ($i=0; $i < count($errors); $i++) { 
           echo $errors[$i];
         }
+        // If there are no errors, run $post->post() and give success message
       } elseif ($post->post($title, $postContent)) {
         echo '<p>Post successful.</p>';
       } 
@@ -47,13 +53,20 @@
 
 <?php
 
+  // If delete button is pressed
   if (isset($_GET['postid'])) {
+    // Get GET data
     $id = $_GET['postid'];
+
+    // New Post Object
     $post = new Post();
+    // Delete post by id
     $post->deletePost($id);
   }
 
+  // Create new Post Object
   $posts = new Post();
+  // Get all posts with mode admin to add delete button
   $posts->getPosts('all', 'admin');
 
 include('includes/footer.php');
