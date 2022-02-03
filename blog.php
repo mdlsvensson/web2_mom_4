7759096b2de54041a5a3a51147adf1a8a0f975c7
mdlsvensson <wilmer.lindau@gmail.com>
2022-02-03 14:40:55 +0100

    blog.php comments

--- blog.php
+++ blog.php
@@ -2,7 +2,9 @@
   $page_title = "All Blog Posts";
   include("includes/header.php");
 
+  // New Post Object
   $posts = new Post();
+  // Get all posts
   $posts->getPosts();
 
   include("includes/footer.php");
