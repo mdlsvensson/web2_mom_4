5bab84832a7889086b37c70334a07f80325352a1
mdlsvensson <wilmer.lindau@gmail.com>
2022-02-03 11:52:57 +0100

    DBH comments

--- classes/Dbh.class.php
+++ classes/Dbh.class.php
@@ -1,5 +1,6 @@
 <?php
 
+// Database handler class with PDO
 class Dbh {
 
   private $host;
@@ -8,19 +9,27 @@ class Dbh {
   private $dbname;
   private $port;
 
+  // Main query function
   public function query($sql) {
+    // Assigning values
     $this->host = '127.0.0.1';
     $this->username = 'root';
     $this->password = '';
     $this->dbname = 'blogdb';
     $this->port = '3306';
 
+    // Database connection
     try {
+      // DSN
       $dsn = 'mysql:dbname=' . $this->dbname . ';host=' . $this->host . ';port=' . $this->port;
+      // Creating PDO connection
       $pdo = new PDO($dsn, $this->username, $this->password);
+      // Setting error mode for PDO to throw exceptions
       $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      // Sending query and returning the result
       $result = $pdo->query($sql);
       return $result;
+      // Catch errors and echo to the screen
     } catch (PDOException $e) {
       echo 'Connection failed: ' . $e->getMessage();
     }
